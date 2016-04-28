d4dd3537d1ac2fd6be024eca3600e0edee20ba68
sh0v0n <shvnbasak@outlook.com>
2016-04-28 20:42:15 +0600

    Game object

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import scenes.gameScene;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        gameScene.show();
     }
 
 
