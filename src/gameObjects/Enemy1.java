886e53197988f51cee970430fc1a8c4af0357000
sh0v0n <shvnbasak@outlook.com>
2016-04-30 13:32:24 +0600

    Runnable Enemy class

--- src/gameObjects/Enemy1.java
+++ src/gameObjects/Enemy1.java
@@ -1,10 +1,16 @@
 package gameObjects;
 
 
+import javafx.application.Platform;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import scenes.GameScene;
 
-public class Enemy1 extends Circle {
+public class Enemy1 extends Circle implements Runnable{
+    GameScene gameScene;
+
+    public boolean horizontalDirection;
+    public boolean verticalDirection;
 
     public double radius;
     public double rightBound;
@@ -16,21 +22,9 @@ public class Enemy1 extends Circle {
     public double speedX;
     public double speedY;
 
+    Thread enemyThread;
 
-    public Enemy1(double radius) {
-        super(radius,Paint.valueOf("red"));
-        radius = this.getRadius();
-        centerX = this.getCenterX();
-        centerY = this.getCenterY();
-        rightBound = centerX + radius;
-        leftBound = centerX - radius;
-        upperBound = centerY - radius;
-        lowerBound = centerY + radius;
-        speedX = 2;
-        speedY = 2;
-    }
-
-    public Enemy1(double centerX, double centerY, double radius) {
+    public Enemy1(double centerX, double centerY, double radius, GameScene gameScene) {
         super(centerX,centerY,radius, Paint.valueOf("red"));
         radius = this.getRadius();
         centerX = this.getCenterX();
@@ -41,12 +35,93 @@ public class Enemy1 extends Circle {
         lowerBound = centerY + radius;
         speedX = 2;
         speedY = 2;
+        this.gameScene = gameScene;
+
+        horizontalDirection = true;
+        verticalDirection = true;
+
+        enemyThread = new Thread(this);
+        enemyThread.start();
+    }
+
+    public void setHorizontalDirection(boolean direction){
+        if(direction){
+            horizontalDirection = true;
+        }
+        else{
+            horizontalDirection = false;
+        }
+    }
+    public void setVerticalDirection(boolean direction){
+        if(direction){
+            verticalDirection = true;
+        }
+        else{
+            verticalDirection = false;
+        }
     }
 
     public void setSpeed(double x){
-        speedX =x;
-        speedY =x;
+        speedX = x;
+        speedY = x;
     }
 
+    public void moveRight(){
+        this.setCenterX(this.centerX + speedX);
+    }
+    public void moveLeft(){
+        this.setCenterX(this.centerX - speedX);
+    }
+    public void moveDown(){
+        this.setCenterY(this.centerY + speedY);
+    }
+    public void moveUp(){
+        this.setCenterY(this.centerY - speedY);
+    }
 
+    public void run() {
+        while(this.isVisible()){
+            Platform.runLater(() -> {
+                this.centerX = this.getCenterX();
+                this.centerY = this.getCenterY();
+                this.rightBound = this.centerX + this.radius;
+                this.leftBound = this.centerX - this.radius;
+                this.upperBound = this.centerY - this.radius;
+                this.lowerBound = this.centerY + this.radius;
+
+
+                if (horizontalDirection) {
+                    if (this.rightBound < gameScene.window.getWidth() - 30) {
+                        this.moveRight();
+                    } else {
+                        horizontalDirection = false;
+                    }
+                } else {
+                    if (this.leftBound > 20) {
+                        this.moveLeft();
+                    } else {
+                        horizontalDirection = true;
+                    }
+                }
+
+
+                if (verticalDirection) {
+                    if (this.lowerBound < gameScene.window.getHeight() - 55) {
+                        this.moveDown();
+                    } else {
+                        verticalDirection = false;
+                    }
+                } else {
+                    if (this.upperBound > 20) {
+                        this.moveUp();
+                    } else {
+                        verticalDirection = true;
+                    }
+                }
+            });
+            try{
+                enemyThread.sleep(5);
+            }catch (Exception e){}
+        }
+    }
 }
