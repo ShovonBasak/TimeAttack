641ecb992fca85cc1eac232a37dfb52758037691
tazim <tazimtazim2012@hotmail.com>
2016-06-18 18:38:03 +0600

    Nothing Important

--- src/Root/scenes/HighScoreScene.java
+++ src/Root/scenes/HighScoreScene.java
@@ -21,6 +21,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 import java.util.ArrayList;
 import java.util.Optional;
